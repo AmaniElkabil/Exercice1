2d74ba30c03fe18e995e86a60f9140696ef5c58e
AmaniElkabil <amani.elkabil@usmba.ac.ma>
2022-10-31 22:57:57 +0100

    Add project files.

---
+++ Exercice1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez un nombre :");
+            int nbr = int.Parse(Console.ReadLine());
+            int i;
+            bool b = true;
+            for (i = 1; i < nbr % 2; i++)
+            {
+                if (nbr % 2 == 0)
+                {
+                    b = false;
+                }
+            }
+            if (b == true)
+            {
+                Console.WriteLine("le nombre est premier");
+            }
+            else
+            {
+                Console.WriteLine("le nombre n 'est pas premier");
+            }
+        }
+    }
+}
+
